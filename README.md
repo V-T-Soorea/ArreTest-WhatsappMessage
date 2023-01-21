5f294d31104bacd88f8f9403ca8378fc9b3f2009
V-T-Soorea <vishnusoorea@gmail.com>
2023-01-21 16:12:47 +0530

    Comments added

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # ArreTest-WhatsappMessage
 
-DB used: MongoDb
+DB used: MongoDb(Mongoose)
 
 To run the application:
 Install node
 Type command: node index.js in terminal
+
+Models Folder contains the schema of the tables
+Config Folder contains the configurations of the db
+Routes Folder contains the api calls 
  
--- routes/apis.js
+++ routes/apis.js
@@ -7,13 +7,16 @@ const User = require("../models/user");
 // Adds message to the message Table in the DB with the sent time of the message, user and group.
 router.post('/postMessage', (req, res) => {
     //get userID and groupID from user and group tables
-    //Use userId and groupID to     
+    //Use userId and groupID to get ugId from UserGroup table
+    //Add ugId,the message and message sent time into messages table
 });
 
 //@route    GET /paginatedMessage
 //Get request for all the message of a particular group in paginated manner
 router.get('/paginatedMessage', async (req, res) => {
-    
+    //Using groupName from the request we can find its groupId from group table
+    //Find all the ugId which are of the given groupId
+    //Find all the messages of these ugId and display the messages in ascending order of time    
 });
 
 module.exports = router;
